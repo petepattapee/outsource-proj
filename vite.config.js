1396ea20d307463e865287ef1d80ee89180c83ad
Pattapee Pimton <lordhomeless77@gmail.com>
2023-06-03 18:22:05 +0700

    gh

--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "nested/index.html"),
-      },
-    },
-  },
   plugins: [react()],
-});
+})
