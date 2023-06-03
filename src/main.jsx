eaa7f29b1048fb8bf79486aaea3a79336327ff01
Pattapee Pimton <lordhomeless77@gmail.com>
2023-06-03 15:56:28 +0700

    Deploy

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { AuthProvider } from "./context/AuthProvider";
 import App from "./App.jsx";
+import { disableReactDevTools } from "@fvilers/disable-react-devtools";
+
+if (process.env.NODE_ENV === "production") {
+  disableReactDevTools();
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
